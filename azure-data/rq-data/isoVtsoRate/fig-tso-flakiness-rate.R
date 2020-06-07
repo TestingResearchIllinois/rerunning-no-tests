0e2a24b6f742d90b7e91d085980e249042126740
Victoria Stodden <victoria@stodden.net>
2020-06-07 12:10:53 -0500

    updated plot commands to match submitted plot

--- azure-data/rq-data/isoVtsoRate/fig-tso-flakiness-rate.R
+++ azure-data/rq-data/isoVtsoRate/fig-tso-flakiness-rate.R
@@ -3,4 +3,6 @@
 plot.data=read.table(file="~/Downloads/no-outlier-simplified.txt",sep=',',header=TRUE)
 plot.data[,2] <- as.numeric(gsub("%", "",plot.data[,2]))/100
 plot.data[,3] <- as.numeric(gsub("%", "",plot.data[,3]))/100
-plot(plot.data$TSO_flake_rate,plot.data$ISO_flake_rate,xlab=("Flakiness rate in test suite"),ylab=("Flakiness rate in isolation"),main="Flakiness rate of all TSO tests (n=82)")
+plot(plot.data$TSO_flake_rate,plot.data$ISO_flake_rate,xlab=("Failure rate in test suite"),ylab=("Failure rate in isolation"),cex.lab=1.5,xaxt="n",yaxt="n")
+axis(2, at=c(0,.01,.02,.03), labels=paste0(c(0,.01,.02,.03)*100, "%"),tick = FALSE,cex.lab=5,cex.axis=1.5)
+axis(1, at=c(0,.01,.02,.0285), labels=paste0(c(0,.01,.02,.03)*100, "%"),tick = FALSE,cex.lab=3,cex.axis=1.5)
